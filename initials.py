870f029555458ae148969b4e090123c8af884caa
Becca-08 <rebeccalamptey006@gmail.com>
2026-05-30 22:40:25 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,44 @@
+# File: initials.py
+# Description: Prits my initials in large styled letters.
+# Assignment Number: 1
+#
+# Name: Rebecca Lamptey
+# STUDENT ID: 2425404320
+# Email: 2425404320@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, Rebecca Lamptey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+  # Print my initials in large styled letters.
+
+  print()
+  print("...RLM")
+  print()
+
+  print("...RRRRRRRRR.....LL..............MMM........MMM.....")
+  print("...RRR.....RRR...LL..............MMMM......MMMM.....")
+  print("...RRR.....RRR...LL..............MMMMM....MMMMM.....")
+  print("...RRR.....RRR...LL..............MMM.MM..MM.MMM.....")
+  print("...RRRRRRRRR.....LL..............MMM..MMMM..MMM.....")
+  print("...RRRRRR........LL..............MMM...MM...MMM.....")
+  print("...RRR..RRR......LL..............MMM........MMM.....")
+  print("...RRR...RRR.....LL..............MMM........MMM.....")
+  print("...RRR.....RRR...LLLLLLLLLLLL....MMM........MMM...**")
+  print("...RRR......RRR..LLLLLLLLLLLL....MMM........MMM...**")
+
+  print()
+
+  main()
+ 
+
+  
+
+
+                                                                                                                                                            ass #code goes here
 
 
 
-main()
+
